f73a065e7b9d50aa5a63682da990a9cebc4d0786
Andrea <a.onofrei@student.utwente.nl>
2025-05-18 18:39:31 +0200

    working version_util

--- libversion/version_util.py
+++ libversion/version_util.py
@@ -1,8 +1,6 @@
 import os
-class VersionUtil:
 
-    @staticmethod
-    def get_version():
-        path = os.path.join(os.path.dirname(__file__), "..", "version.txt")
-        with open(path) as f:
-            return f.read().strip()
+def get_version():
+    path = os.path.join(os.path.dirname(__file__), "..", "version.txt")
+    with open(path) as f:
+        return f.read().strip()
