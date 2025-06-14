e657a1189a443ce747719e06d4804776cbb59e18
Sejidz <seyidalibulut@tudelft.nl>
2025-06-14 15:55:27 +0200

    Update __init__.py

--- libversion/__init__.py
+++ libversion/__init__.py
@@ -1,1 +1,7 @@
-from .version_util import get_version
+from .version_util import VersionUtil
+
+__all__ = ["VersionUtil", "get_version"]
+
+def get_version() -> str:
+    #get the version using the VersionUtil classs
+    return VersionUtil().get_version()
